231162809fdb8c80437c2dd5a00d06ffbb42bcdd
Dewan Minhajul Islam <dewan.minhajul@gmail.com>
2026-09-25 12:42:33 +0600

    Form Create and insert data into table from user using POST.get() in ORM

--- ims_project/products/urls.py
+++ ims_project/products/urls.py
@@ -5,4 +5,5 @@ from products.views import *
 urlpatterns = [
     path('', home_view, name='home_page'),
     path('all-products/', product_view, name='products_page'),
+    path('add-product/', product_form, name='add_product_page'),
 ]
--- ims_project/products/views.py
+++ ims_project/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from products.models import ProductModel
 
 # Create your views here.
@@ -16,3 +16,26 @@ def product_view(request):
     }
     
     return render(request, 'product-list.html', context)
+
+
+def product_form(request):
+    
+    if request.method == 'POST':
+        
+        vp_name = request.POST.get('p_name')
+        vp_description = request.POST.get('p_description')
+        vp_date = request.POST.get('p_date')
+        vp_type = request.POST.get('p_type')
+        vp_image = request.FILES.get('p_image')
+        
+        ProductModel.objects.create(
+            product_name = vp_name,
+            description = vp_description,
+            production_date = vp_date,
+            product_type = vp_type,
+            image = vp_image
+        )
+        
+        return redirect('products_page')
+    
+    return render(request, 'add-product.html')
